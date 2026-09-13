fdcf76e3cf10fba9850baa15fe1ef080f65e8b3d
Patrick Bittig <bittig.patrick@gmail.com>
2026-09-13 14:20:37 -0500

    refine: use compact verification icon

--- components/space/VerifiedBadge.tsx
+++ components/space/VerifiedBadge.tsx
@@ -1,9 +1,8 @@
 export function VerifiedBadge() {
   return (
-    <span className="inline-flex items-center gap-1 rounded-full bg-workcofy-green/20 px-2.5 py-1 text-xs font-semibold text-workcofy-black">
+    <span className="inline-flex items-center" title="Verificado" aria-label="Verificado">
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="/icons/nav-check.png" alt="" className="h-3.5 w-3.5" />
-      Verificado
+      <img src="/icons/verificado.png" alt="Verificado" className="h-7 w-7 object-contain" />
     </span>
   )
 }
